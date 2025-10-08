d88cf257226d7075dcd3997319c3d65123b34a47
Nikos Nikolaidis <nikosnikolaidis@users.noreply.github.com>
2025-10-08 11:35:29 +0300

    Remove CrossOrigin from SkillScoreController

--- src/main/java/com/example/hiringProcess/SkillScore/SkillScoreController.java
+++ src/main/java/com/example/hiringProcess/SkillScore/SkillScoreController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1/skill-scores")
 public class SkillScoreController {
