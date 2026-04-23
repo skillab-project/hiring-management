242947262812128b29628f617f181b76ce8af907
teomaik <teomaik19@gmail.com>
2026-04-23 14:40:34 +0300

    Update SkillScoreController.java

--- src/main/java/com/example/hiringProcess/SkillScore/SkillScoreController.java
+++ src/main/java/com/example/hiringProcess/SkillScore/SkillScoreController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/organizations/{orgId}/skill-scores")
+@RequestMapping("/api/v1/skill-scores")
 public class SkillScoreController {
 
     private final SkillScoreService skillScoreService;
